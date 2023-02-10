907bb46958c43a05ad1a1d7f04aedf92e557bd09
INS140 <IsaacNSaettel@gmail.com>
2023-02-09 21:33:17 -0800

    places index page

--- views/default.jsx
+++ views/default.jsx
@@ -7,7 +7,6 @@ function Def(props) {
                 <meta charSet="UTF-8" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <link rel="stylesheet" href="../styles.css" />
                 <title>REST-rant</title>
             </head>
             <body>
