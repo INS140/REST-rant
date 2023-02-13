3ba498a0f80c7d5c8084e21223f27f176cb96f27
INS140 <IsaacNSaettel@gmail.com>
2023-02-13 03:47:30 -0800

    added bootstrap and local css

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -13,4 +17,6 @@ app.get('/', (req, res) => {
 
 app.get('*', (req, res) => res.status(404).render('Error404'))
 
+
+// Server Listeners
 app.listen(process.env.PORT)
--- views/default.jsx
+++ views/default.jsx
@@ -7,6 +7,8 @@ function Def(props) {
                 <meta charSet="UTF-8" />
                 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous" />
+                <link rel="stylesheet" href="/css/styles.css" />
                 <title>REST-rant</title>
             </head>
             <body>
