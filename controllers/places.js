69584777c22c62abded28ee388bde5bc9a48781d
INS140 <IsaacNSaettel@gmail.com>
2023-02-14 02:44:53 -0800

    add form and post route

--- controllers/places.js
+++ controllers/places.js
@@ -1,26 +1,25 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    const places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/h-thai-ml.jpg',
-        source: 'Trip101',
-        attr: 'https://trip101.com/article/fancy-restaurants-in-las-vegas'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/cat-cafe.jpg',
-        source: 'Ecurrent',
-        attr: 'https://www.ecurrent.com/news/were-getting-a-cat-cafe/'
-      }]
-      
     res.render('places/index', { places })
 })
+
+// CREATE
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    req.body.pic = '/images/default-restaurant.png'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+
 
 router.get('/new', (req, res) => {
   res.render('places/new')
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -12,12 +12,15 @@ function Index(props) {
                             <div key={index} className="col-sm-6">
                                 <h2>{place.name}</h2>
                                 <p className='text-center'>{place.cuisines}</p>
-                                <div className='img-container'>
-                                    <img className="restaurant-img" src={place.pic} alt={place.name} />
-                                    <div className='attribution'>
-                                        Image sourced from <a href={place.attr}>{place.source}</a>
+                                {(place.pic !== '/images/default-restaurant.png')
+                                    ? <div className='img-container'>
+                                        <img className="restaurant-img" src={place.pic} alt={place.name} />
+                                        <div className='attribution'>
+                                            Image sourced from <a href={place.attr}>{place.source}</a>
+                                        </div>
                                     </div>
-                                </div>
+                                    : <img className="restaurant-img" src={place.pic} alt={place.name} />
+                                }
                                 <p className='text-center'>Located in {place.city}, {place.state}</p>
                             </div>
                         )
